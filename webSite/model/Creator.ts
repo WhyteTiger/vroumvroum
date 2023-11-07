e070045fbd9efa8cf68328fc117664d4cfe181b5
Melvyn <melvyn.bauvent@etu.univ-lyon1.fr>
2023-11-07 12:42:31 +0100

    ajout d'un attribut

--- webSite/model/Creator.ts
+++ webSite/model/Creator.ts
@@ -3,13 +3,14 @@ import {List} from "./List";
 import {Kart} from "./Kart";
 
 export class Creator extends User{
-    pseudo     : string;
-    passWord   : string;
-    vroumCoins : number;
-    follows    : List<Creator>
+    pseudo          : string;
+    passWord        : string;
+    vroumCoins      : number;
+    follows         : List<Creator>
+    isAdministrator : boolean;
 
 
-    constructor(kartUsed: Kart, kartList: List<Kart>, pseudo: string, passWord: string, follows: List<Creator>) {
+    constructor(kartUsed: Kart, kartList: List<Kart>, pseudo: string, passWord: string, follows: List<Creator>, isAdministrator: boolean) {
         super(kartUsed, kartList);
         this.pseudo     = pseudo;
         this.passWord   = passWord;
@@ -20,6 +21,7 @@ export class Creator extends User{
             this.follows = follows;
         }
 
-        this.vroumCoins = 0;
+        this.vroumCoins      = 0;
+        this.isAdministrator = isAdministrator;
     }
 }
