30e24de3db167bf40c160150148c580e1f0c742f
Melvyn <melvyn.bauvent@etu.univ-lyon1.fr>
2023-11-30 14:39:30 +0100

    Le controller de connection marche

--- webSite/controllers/controllerConnection.js
+++ webSite/controllers/controllerConnection.js
@@ -2,9 +2,9 @@ import {API} from "../models/API.js";
 
 console.log("Hello\n");
 
-window.localStorage.setItem("success", 		  false);
 window.localStorage.setItem("alreadyRegister", false);
 window.localStorage.setItem("rightPassword",   false);
+window.localStorage.setItem("isConnected",     false);
 window.localStorage.setItem("username", 	 	  "");
 
 async function tryToConnect(username, password) {
@@ -29,26 +29,26 @@ async function tryToConnect(username, password) {
 		.then((response) => response.json())
 		.then((data) => {
 			console.log(data);
-			window.localStorage.setItem("success", 		  data.successOut);
 			window.localStorage.setItem("alreadyRegister", data.alreadyRegisterOut);
 			window.localStorage.setItem("rightPassword",   data.rightPasswordOut);
 			window.localStorage.setItem("username", 	 	  data.usernameOut);
 			
-			console.log(window.localStorage);
-			console.log("isSuccess ? " 		  + window.localStorage.success);
 			console.log("isAlreadyRegister ? " + window.localStorage.alreadyRegister);
 			console.log("isRightPassword ? "   + window.localStorage.rightPassword);
-			console.log("pseudo "				  + window.localStorage.username);
+			console.log("pseudo : \'"			  + window.localStorage.username + "\'");
 			
-			if (window.localStorage.alreadyRegister === true && window.localStorage.rightPassword === true) {
-				console.log("is connected");
+			if (window.localStorage.alreadyRegister === "true" && window.localStorage.rightPassword === "true") {
+				window.localStorage.isConnected = true;
+				
+				console.log("is connected ? "+window.localStorage.isConnected);
+				
 				document.location.href="../views/index.php";
 			} else {
-				console.log("is not connected");
+				console.log("Connection failed");
 			}
 		})
 		.catch(() => {
-			console.log("Fetch error");
+			console.log("Fetch failed");
 		})
 }
 
