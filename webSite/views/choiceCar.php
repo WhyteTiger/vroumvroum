dbc24f1e7ad0dd5a2b33decd43bb82df221f52ef
Melvyn <melvyn.bauvent@etu.univ-lyon1.fr>
2023-12-03 16:45:41 +0100

    Rajout ChoiceKart.js

--- webSite/views/choiceCar.php
+++ webSite/views/choiceCar.php
@@ -5,7 +5,7 @@
     <link rel="icon" href="../../assets/logo.png">
     <link rel="stylesheet" href="styles/generalStyle.css">
     <link rel="stylesheet" href="styles/styleCanvas.css">
-     <script src="../models/ChoiceKart.js"></script>
+     <script type="module" src="../models/ChoiceKart.js"></script>
     <title>Vroumvroum - choix voiture</title>
 </head>
 <body>
