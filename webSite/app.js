e7e9d00bdf70956520beb2a96b09777ef2debf01
BOURBON MEHDI p2205512 <mehdi.bourbon@etu.univ-lyon1.fr>
2023-12-04 01:35:10 +0100

    Refactoring webSite to website

--- webSite/app.js
+++ webSite/app.js
@@ -6,7 +6,7 @@ window.localStorage.setItem("username", 		  "");
 window.localStorage.setItem("alreadyRegister", "");
 window.localStorage.setItem("circuitId",       "1");
 
-document.location.href="./webSite/views/index.html";
+document.location.href="./website/views/index.html";
 
 const text = document.body.getElementById("text");
 text.innerText = "Error 500 : Un problème est survenue lors du chargement de la page...";
