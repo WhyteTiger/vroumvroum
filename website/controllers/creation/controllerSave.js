0387f31cfe3d683c12ff8dbbc6363f97a9b64308
Metro-Lun <mehdi.bourbon@outlook.fr>
2023-12-29 17:27:38 +0100

    début du code JS de l'aside mais j'ai besoin de l'API

---
+++ website/controllers/creation/controllerSave.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('aside button');
+
+btn.addEventListener('click', (evt) => {
+    const name = document.querySelector('aside input').value;
+
+    if(name !== '') {
+        
+    }
+});
