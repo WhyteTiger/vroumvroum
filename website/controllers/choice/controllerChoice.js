58cd92be962b063289870f39399984cf0687f69f
Metro-Lun <mehdi.bourbon@outlook.fr>
2024-01-06 00:07:57 +0100

    ajout zone choix des pages de circuit + CSS + controller

---
+++ website/controllers/choice/controllerChoice.js
@@ -0,0 +1,15 @@
+document.querySelector('.fa-backward-step').addEventListener('click', (evt) => {
+    console.log('revenir a la premiere page')
+});
+
+document.querySelector('.fa-backward').addEventListener('click', (evt) => {
+    console.log('reculer d une page')
+});
+
+document.querySelector('.fa-forward').addEventListener('click', (evt) => {
+    console.log('avancer d une page')
+});
+
+document.querySelector('.fa-forward-step').addEventListener('click', (evt) => {
+    console.log('aller a la derniere page')
+});
