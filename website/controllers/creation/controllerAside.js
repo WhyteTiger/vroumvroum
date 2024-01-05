180ed26d7c81b7a1d6cab1c0a09995328f21c6ee
Metro-Lun <mehdi.bourbon@outlook.fr>
2024-01-05 23:42:29 +0100

    modifs sur le controller aside

--- website/controllers/creation/controllerAside.js
+++ website/controllers/creation/controllerAside.js
@@ -2,91 +2,80 @@ import {API} from "../../models/API.js";
 
 
 console.log('aside')
+console.log(localStorage)
 console.log(localStorage.isConnected)
 
 /* NOTE : THIS SCRIPT APPLIES TO ALL ASIDES IN THE APP, EXPLAINING WHY THERE ARE SO MANY IF STATEMENTS */
 
-window.onload = () => {
-    if(document.querySelector('#savebutton') === null) {    // means we're on the choice page
-        console.log(document.querySelector('aside button'))
+if(document.querySelector('#savebutton') === null) {    // means we're on the choice page
 
-        const boxes = document.querySelectorAll('.circuit-box');
+    // adding an eventListener to all circuit boxes
+    document.querySelectorAll('.circuit-box').forEach((box) => {
+        box.addEventListener('click', (evt) => {
+            const circuitId = box.getAttribute('name');
+            console.log(circuitId)
+            console.log
+            localStorage.circuitId = circuitId;
 
-        boxes.forEach((box) => {
-            box.addEventListener('click', (evt) => {
-                    const circuitId = box.getAttribute('name');
-                    console.log(circuitId)
-                    console.log
-                    localStorage.circuitId = circuitId;
+            document.querySelector('#empty').classList.add('invisible');
+            document.querySelector('#full').classList.remove('invisible');
 
-                    document.querySelector('#empty').classList.add('invisible');
-                    document.querySelector('#full').classList.remove('invisible');
-
-                    const url = API.getURLgetCircuitInformation();
-                    const dataCircuit = {
-                        circuitIdIn: circuitId
-                    };
-                    const params = {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify(dataCircuit)
-                    };
+            const url = API.getURLgetCircuitInformation();
+            const dataCircuit = {
+                circuitIdIn: circuitId
+            };
+            const params = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(dataCircuit)
+            };
+            
+            console.log(params);
+            
+            fetch(url, params)
+                .then((response) => response.json())
+                .then((dataCircuit) => {
+                    console.log(dataCircuit);
                     
-                    console.log(params);
-                    
-                    fetch(url, params)
-                        .then((response) => response.json())
-                        .then((dataCircuit) => {
-                            console.log(dataCircuit);
-                            
-                            const circuitName = document.getElementById("circuitName");
-                            circuitName.innerText = dataCircuit.circuitName;
-                            
-                            console.log("circuitScore : " + dataCircuit.circuitScore);
-                            
-                            const creatorName = document.getElementById("creatorName");
-                            creatorName.innerText = "Créateur : " + dataCircuit.creatorUsername;
-                            
-                            const creatorScore = document.getElementById("creatorScore");
-                            creatorScore.innerText = "Médaille auteur : " + dataCircuit.creatorTime;
+                    document.getElementById("circuit-name").innerText = dataCircuit.circuitName;
+                    document.getElementById("creator-name").innerText = "Créateur : " + dataCircuit.creatorUsername;
+                    document.getElementById("creator-score").innerText = "Médaille auteur : " + dataCircuit.creatorTime;
 
-                            // to manage the 5 (or less) best scores
-                            if(dataCircuit.leaderBoard == null) {
-                                document.querySelector("#leaderboard-players").textContent = "Aucun joueur n'a encore joué à ce circuit. Soyez le premier !";
-                            } else {
-                                let i = 0;
-                                for (let player in document.querySelector('#leaderboard-players p')) {
-                                player.textContent = dataCircuit.leaderBoard[i] + " : " + dataCircuit.leaderBoard[i+1];
-                                i += 2;
-                                }
-                            }
-                        }).catch((error) => console.log(error));
-            });
+                    // to manage the 5 (or less) best scores
+                    if(dataCircuit.leaderBoard == null) {
+                        document.querySelector("#leaderboard-players").textContent = "Aucun joueur n'a encore joué à ce circuit. Soyez le premier !";
+                    } else {
+                        let i = 0;
+                        for (let player in document.querySelector('#leaderboard-players p')) {
+                            player.textContent = dataCircuit.leaderBoard[i] + " : " + dataCircuit.leaderBoard[i+1];
+                            i += 2;
+                        }
+                    }
+                }).catch((error) => console.log(error));
         });
-        
-
-
-
-
-
-
-
-    } else {
-
-    }
-
-
-    if(!localStorage.isConnected) {
-
-    }
-
-
-
+    });
     
 
-    
+
+
+} else {    // means we're on the creation page
+    console.log(document.querySelector('#savebutton'))
+    document.querySelector('#savebutton').addEventListener('click', (evt) => {
+
+        console.log(document.querySelector('aside input').value)
+        console.log(localStorage.playerId)
+
+        // TODO API : enregistrer le fichier avec l'id de l'user et matrix (à transformer au passage pour que ça rentre dans la DB)
+
+    });
 }
+
+
+
+
+
+
 
 
