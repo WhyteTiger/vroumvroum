b0a856fb09dee8b3afd45dd944e976ae2ecf8c3c
Metro-Lun <mehdi.bourbon@outlook.fr>
2024-01-07 17:09:26 +0100

    gestion selecteur de page terminée (il me faudra des circuits en plus)

--- website/controllers/choice/controllerChoice.js
+++ website/controllers/choice/controllerChoice.js
@@ -1,37 +1,12 @@
 import {API} from "../../models/API.js";
 
-console.log(localStorage)
-
-let fetchParams = {
-    personnalCircuitIn: false
-};
-
-let params = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify(fetchParams)
-};
-
-fetch(API.getURLpostCircuitsNumber(), params)
-.then((response) => response.json())
-.then((dataNb) => {
-    let nbCircuits = dataNb.result.circuitnumber;
-
-    // let's create the boxes
-    for(let i = 0; i < nbCircuits; i++) {
-        const box = document.createElement('div');
-        box.classList.add('circuit-box');
-        document.querySelector('#circuits').appendChild(box);
-    }
-
-    const nbPages = Math.ceil(nbCircuits / 12)
+function fetchPage(nb) {
+    while(document.querySelector('#circuits').firstChild) document.querySelector('#circuits').removeChild(document.querySelector('#circuits').firstChild);
 
     // fetch the circuits
     fetchParams = {
         personnalCircuitIn: false,
-        pageNumberIn: 1
+        pageNumberIn: nb
     };
     
     params = {
@@ -46,16 +21,23 @@ fetch(API.getURLpostCircuitsNumber(), params)
     .then((response) => response.json())
     .then((dataCircuits) => {
         console.log('PARTIE 2')
-        console.log(dataCircuits.result)
+        console.log(dataCircuits.result.length)
         const circuits = dataCircuits.result;
+
+        // let's create the boxes
+        for(let i = 0; i < circuits.length; i++) {
+            const box = document.createElement('div');
+            box.classList.add('circuit-box');
+            document.querySelector('#circuits').appendChild(box);
+        }
 
         const boxList = document.querySelectorAll('.circuit-box');
 
         for(let i = 0; i < boxList.length; i++) {
-            boxList[i].setAttribute("name", circuits[i].circuitid)
+            boxList[i].setAttribute("name", circuits[i].circuitid);
 
             const p = document.createElement('p');
-            p.textContent = circuits[i].circuitname
+            p.textContent = circuits[i].circuitname;
             boxList[i].appendChild(p)
 
             boxList[i].addEventListener('click', (evt) => {
@@ -99,37 +81,61 @@ fetch(API.getURLpostCircuitsNumber(), params)
                     }
                 })
                 .catch((err) => console.log(`error : dataCircuit : ${err}`));
-
             });
         }
-
     })
-    .catch((error) => console.log(`error : circuits : ${error}`));
+    .catch((error) => console.log(`fetch error : nbCircuits : ${error}`));
+}
 
 
+/* MAIN PART OF THE SCRIPT */
 
+let fetchParams = {
+    personnalCircuitIn: false
+};
 
+let params = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(fetchParams)
+};
 
+fetch(API.getURLpostCircuitsNumber(), params)
+.then((response) => response.json())
+.then((dataNb) => {
+    const nbCircuits = dataNb.result.circuitnumber;
+    const nbPages = Math.ceil(nbCircuits / 12);
 
+    // à décommenter plus tard
+    //if(nbPages === 1) document.querySelector('#page-selector').classList.add('invisible');
 
-    // nbCircuits = data.result
+    let currentPage = 1;
+
+    fetchPage(1);
+
+    // eventListeners for the page selector, only if there is more than 1 page
+    //if(nbPages > 1) {
+        document.querySelector('.fa-backward-step').addEventListener('click', (evt) => {    // go back to the 1st page
+            fetchPage(1);
+        });
+        
+        document.querySelector('.fa-backward').addEventListener('click', (evt) => {
+            if(currentPage > 1) fetchPage(--currentPage);
+        });
+        
+        document.querySelector('.fa-forward').addEventListener('click', (evt) => {
+            if(currentPage < nbPages) fetchPage(++currentPage);
+        });
+        
+        document.querySelector('.fa-forward-step').addEventListener('click', (evt) => {
+            fetchPage(nbPages);
+        });
+   // }
+
+        
+
 })
-.catch((error) => console.log(`fetch error : nbCircuits : ${error}`));
+.catch((error) => console.log(`error : circuits : ${error}`));
 
-
-
-document.querySelector('.fa-backward-step').addEventListener('click', (evt) => {
-    console.log('revenir a la premiere page')
-});
-
-document.querySelector('.fa-backward').addEventListener('click', (evt) => {
-    console.log('reculer d une page')
-});
-
-document.querySelector('.fa-forward').addEventListener('click', (evt) => {
-    console.log('avancer d une page')
-});
-
-document.querySelector('.fa-forward-step').addEventListener('click', (evt) => {
-    console.log('aller a la derniere page')
-});
