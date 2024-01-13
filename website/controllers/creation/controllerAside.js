5e12cba2cb8ec4257205699808e0f025a4f43f61
Melvyn <melvyn.bauvent@etu.univ-lyon1.fr>
2024-01-13 17:27:08 +0100

    Remise en état de marche le controllerAside.js

--- website/controllers/creation/controllerAside.js
+++ website/controllers/creation/controllerAside.js
@@ -1,6 +1,25 @@
+import {API} from "../../models/API.js";
+
 console.log('aside')
+console.log(localStorage)
+console.log(localStorage.isConnected)
 
+/* NOTE : THIS SCRIPT APPLIES TO ALL ASIDES IN THE APP, EXPLAINING WHY THERE ARE SO MANY IF STATEMENTS */
 
-// récupération du nom entré dans la pop-up
-const nameCircuit = window.localStorage.inputField;
-console.log(nameCircuit);
+if(document.querySelector('#savebutton') === null) {    // means we're on the choice page
+	
+	document.querySelector('#playbutton').addEventListener('click', (evt) => {
+		document.location.href = 'playCircuit.html';
+	});
+	
+} else {    // means we're on the creation page
+	console.log(document.querySelector('#savebutton'))
+	document.querySelector('#savebutton').addEventListener('click', (evt) => {
+		
+		console.log(document.querySelector('aside input').value)
+		console.log(localStorage.playerId)
+		
+		// TODO API : enregistrer le fichier avec l'id de l'user et matrix (à transformer au passage pour que ça rentre dans la DB)
+		
+	});
+}
