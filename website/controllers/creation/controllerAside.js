5435e05c83b6e3ad4db764f90de6f2dad420ceae
Metro-Lun <mehdi.bourbon@outlook.fr>
2024-01-14 21:14:35 +0100

    je saurais plus dire ce que j'ai fait mais j'ai fait des trucs

--- website/controllers/creation/controllerAside.js
+++ website/controllers/creation/controllerAside.js
@@ -1,10 +1,11 @@
-import {API} from "../../models/API.js";
+import { API } from "../../models/API.js";
+import { Alert } from "../../models/entities/Alert.js";
 
 console.log('aside')
 console.log(localStorage)
 console.log(localStorage.isConnected)
 
-/* NOTE : THIS SCRIPT APPLIES TO ALL ASIDES IN THE APP, EXPLAINING WHY THERE ARE SO MANY IF STATEMENTS */
+/* NOTE : THIS SCRIPT APPLIES TO ALL ASIDES IN THE APP, EXPLAINING WHY THERE ARE "SO MANY" IF STATEMENTS */
 
 if(document.querySelector('#savebutton') === null) {    // means we're on the choice page
 	
@@ -19,7 +20,43 @@ if(document.querySelector('#savebutton') === null) {    // means we're on the ch
 		console.log(document.querySelector('aside input').value)
 		console.log(localStorage.playerId)
 		
-		// TODO API : enregistrer le fichier avec l'id de l'user et matrix (à transformer au passage pour que ça rentre dans la DB)
+		const popUp = new Alert("Voulez vous sauvegarder votre circuit ?", "Sauvegarder", "", 'save');
+		popUp.customAlert();
+
+		console.log("melvyn")
+		console.log(localStorage.getItem("creatorTime"))
 		
+		const playerIdIn    = localStorage.getItem("playerId");
+		const matrixIn      = JSON.parse(localStorage.getItem('matrix'));
+		const circuitNameIn = localStorage.getItem("circuitName");
+		const creatorTimeIn = localStorage.getItem("creatorTime") === null ? 0 : localStorage.getItem("creatorTime");
+		const circuitLapsIn = localStorage.getItem("circuitLaps");
+		
+		const dataCircuit = {
+			playerIdIn,
+			matrixIn,
+			circuitNameIn,
+			creatorTimeIn,
+			circuitLapsIn
+		};
+		const params = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(dataCircuit)
+		};
+		console.log(params);
+		
+		fetch(API.getURLpostCircuitOfPlayerId(), params)
+			.then((response) => response.json())
+			.then((dataCircuit) => {
+				console.log(dataCircuit);
+				if (dataCircuit.success === "true") {
+					console.log("saved successfully");
+				} else {
+					console.error("saved error");
+				}
+			});
 	});
 }
