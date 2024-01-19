ac20080b7149d0a91d33e98f6ea86b62be84f29e
Camille <camille.morfin@etu.univ-lyon1.fr>
2024-01-19 22:29:24 +0100

    affichage mdp + affichage loarder bar

--- website/models/creation/loaderBar.js
+++ website/models/creation/loaderBar.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const header = document.querySelector("header");
 
     if (valBar === "0"){
+        loaderContainer.style.display = "flex";
         header.style.display = "none";
         let width = 0;
         let interval = setInterval(function () {
