77830c5d4d7f436fcc94f1ee3cab8ae4faddc418
Metro-Lun <mehdi.bourbon@outlook.fr>
2024-01-21 19:36:13 +0100

    nettoyage et validation W3C des fichiers HTML

--- website/controllers/redirection/controllerHome.js
+++ website/controllers/redirection/controllerHome.js
@@ -28,8 +28,8 @@ createCircuit.addEventListener("click", () => {
 		const newAlert = new Alert("Vous ne pouvez pas accéder à cette page si vous n'êtes pas connecté !", "Se connecter", "connection.html" , 'warning');
 		newAlert.customAlert();
 	} else {
-		localStorage.setItem("personal", "true");
-		document.location.href = "choiceCircuit.html";
+		localStorage.setItem("personal", "false");
+		document.location.href = "createCircuit.html";
 	}
 });
 
