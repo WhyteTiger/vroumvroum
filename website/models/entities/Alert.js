bc387540f88ab004d29195124f66e7c210e1cdb6
Camille <camille.morfin@etu.univ-lyon1.fr>
2024-03-21 13:36:53 +0100

    verification mdp correct pour changement

--- website/models/entities/Alert.js
+++ website/models/entities/Alert.js
@@ -597,6 +597,7 @@ export class Alert{
         fetch(url, params)
            .then((response) => response.json())
            .then((result) => {
+               console.log(result);
            })
            .catch((err) => {
                 console.error(err);
